e4d407110121c866fa157039281b9f4cee29eaa1
Ilyane Fox <85144115+ilyanefox@users.noreply.github.com>
2022-01-04 16:06:58 -0600

    added front end project in Angular

---
+++ webapp/employeemanagerapp/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import {Component, OnInit} from '@angular/core';
+
+import {Employee} from "./employee";
+import {EmployeeService} from "./employee.service";
+import {HttpErrorResponse} from "@angular/common/http";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  public employees: Employee[] = [];
+
+  constructor(private employeeService: EmployeeService) {
+  }
+
+  ngOnInit() {
+    this.getEmployees();
+  }
+
+  public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe({
+      next: (response: Employee[]) => {
+        this.employees = response;
+      },
+      error: (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    });
+  }
+}
